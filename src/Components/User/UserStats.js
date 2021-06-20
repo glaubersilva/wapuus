7bc4c92145bb76d534989a6cb896a374c3368ba0
Glauber Silva <info@glaubersilva.me>
2021-06-20 16:58:32 -0300

    React Lazy for Graphs

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -4,7 +4,8 @@ import useFetch from '../../Hooks/useFetch';
 import { STATS_GET } from '../../api';
 import Loading from '../Helpers/Loading';
 import Error from '../Helpers/Error';
-import UserStatsGraphs from './UserStatsGraphs';
+//import UserStatsGraphs from './UserStatsGraphs';
+const UserStatsGraphs = React.lazy( () => import('./UserStatsGraphs'));
 
 const UserStats = () => {
   const { data, error, loading, request } = useFetch();
@@ -21,10 +22,10 @@ const UserStats = () => {
   if (error) return <Error message={error} />;
   if (data)
     return (
-      <div>
+      <React.Suspense fallback={<div></div>}>
         <Head title="Stats" />
         <UserStatsGraphs data={data} />
-      </div>
+      </React.Suspense>
     );
   else return null;
 };
